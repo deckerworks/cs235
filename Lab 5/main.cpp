70162464a6ff3a99c50c4bc6a125b48e22f11d49
josephdecker1 <josephdecker1@gmail.com>
2016-02-27 03:33:31 +0000

    updated lab 4

--- Lab 5/main.cpp
+++ Lab 5/main.cpp
@@ -2,7 +2,15 @@
 
 using namespace std;
 
-int main() {
+void print_num(int num) {
+    
+}
+
+int main() {s
     
     cout << "THIS IS A PROGRAM" << endl;
+    
+    print_num(5);
+    return 0;
+    
 }
